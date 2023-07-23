23a2b06992a181ae6f5f3ff04f9b0ea627333840
mariyaGorobec <mariyagorobec.2001@gmail.com>
2023-07-23 17:46:34 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import style from "./index.module.scss";
 
 function App() {
+  let arr = [];
+  for (let i = 0; i < 100; i++) {
+    arr.push(1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.conteiner}>
+      <div class={style.grid}>
+        {arr.map((item) => (
+          <div></div>
+        ))}
+        <div className={style.item}>
+          <img
+            height={350}
+            width={500}
+            className={style.imgSalon}
+            src="/img/imgSalon.png"
+          ></img>
+          <h3>лучшие ноготочки в твоём городе!</h3>
+          <h1> ПО РУКАМ</h1>
+          <div className={style.quote}> 
+          <span className={style.quoteAboutManicure}>«Всё в ваших руках. Поэтому они должны быть в полном порядке» — Coco Chanel</span>
+          <span className={style.location}>г.Иркутск, ул.Байкальская, д.2, тел. +7(999)-999-99-99</span>
+        </div>
+        </div>
+        
+      </div>
+    
     </div>
   );
 }
