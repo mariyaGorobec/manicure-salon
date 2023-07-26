d53da97c8735249f35dbdaf14be03d7609a63ef4
mariyaGorobec <mariyagorobec.2001@gmail.com>
2023-07-26 19:18:31 +0800

    create menu

--- src/App.js
+++ src/App.js
@@ -3,33 +3,62 @@ import style from "./index.module.scss";
 
 function App() {
   let arr = [];
+  let menuItem = [
+    "Кто мы?",
+    "Сколько стоит?",
+    "Фоточки наших работ",
+    "А свободные окошки есть?",
+  ];
   for (let i = 0; i < 100; i++) {
     arr.push(1);
   }
 
   return (
     <div className={style.conteiner}>
-      <img className={style.arms} width={400} height={350} src="/img/arms.png"></img>
-      
+      <img
+        className={style.arms}
+        width={400}
+        height={350}
+        src="/img/arms.png"
+      ></img>
+
       <div class={style.grid}>
         {arr.map((item) => (
           <div></div>
         ))}
         <div className={style.item}>
           <div className={style.imagesForDesign}>
-          
-         
-          <img
-            height={300}
-            width={450}
-            className={style.imgSalon}
-            src="/img/imgSalon.png"
-          ></img>
-           <img className={style.circle} height={300} width={300} src="/img/circle.png"></img>
-           
-          <img className={style.butterfly} height={150} width={200} src="/img/butterfly.png"></img>
-          <img className={style.heart} src = '/img/heart.png' width={160} height={150}></img>
-          <img className={style.mobilePhone} width={70} height={120} src="/img/mobilePhone.png"></img>
+            <img
+              height={300}
+              width={450}
+              className={style.imgSalon}
+              src="/img/imgSalon.png"
+            ></img>
+            <img
+              className={style.circle}
+              height={300}
+              width={300}
+              src="/img/circle.png"
+            ></img>
+
+            <img
+              className={style.butterfly}
+              height={150}
+              width={200}
+              src="/img/butterfly.png"
+            ></img>
+            <img
+              className={style.heart}
+              src="/img/heart.png"
+              width={160}
+              height={150}
+            ></img>
+            <img
+              className={style.mobilePhone}
+              width={70}
+              height={120}
+              src="/img/mobilePhone.png"
+            ></img>
           </div>
           <h3>лучшие ноготочки в твоём городе!</h3>
           <h1> ПО РУКАМ</h1>
@@ -44,7 +73,24 @@ function App() {
           </div>
         </div>
       </div>
-      <img className={style.bear} width={250} height={250} src="/img/bear.png"></img>
+     <div className={style.bearAndMenu}>
+     <img
+        className={style.bear}
+        width={250}
+        height={250}
+        src="/img/bear.png"
+      ></img>
+      <div className={style.menu}>
+        <ul>
+          {menuItem.map((item) => (
+            <li>
+              <img src="/img/star.png" width={50} height={50}></img>
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+     </div>
     </div>
   );
 }
