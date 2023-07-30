77b079eac4b5066f70a7a4164cc2d48151d0501e
mariyaGorobec <mariyagorobec.2001@gmail.com>
2023-07-30 15:05:45 +0800

    Инициализация redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.module.scss';
-import {Routes,Route,BrowserRouter as Router} from 'react-router-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.module.scss";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 ReactDOM.render(
-  <Router>
-    <Routes>
-      <Route  path="*" element={<App />} />
-    </Routes>
-  </Router>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
 
 reportWebVitals();
